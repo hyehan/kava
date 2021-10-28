7c9fdb1215a0c542a92eacd30412d913fcdca8f8
denalimarsh <denalimarsh@gmail.com>
2021-10-28 10:55:54 -0700

    update codec, expected keepers

--- x/auction/types/codec.go
+++ x/auction/types/codec.go
@@ -2,22 +2,48 @@ package types
 
 import (
 	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/cosmos/cosmos-sdk/codec/types"
+
+	cryptocodec "github.com/cosmos/cosmos-sdk/crypto/codec"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/msgservice"
+
+	"github.com/kava-labs/kava/x/auction/exported"
 )
 
-// ModuleCdc module level codec
-var ModuleCdc = codec.New()
+// RegisterLegacyAminoCodec registers all the necessary types and interfaces for the
+// governance module.
+func RegisterLegacyAminoCodec(cdc *codec.LegacyAmino) {
+	cdc.RegisterConcrete(&MsgPlaceBid{}, "auction/MsgPlaceBid", nil)
 
-func init() {
-	RegisterCodec(ModuleCdc)
+	cdc.RegisterInterface((*exported.GenesisAuction)(nil), nil)
+	cdc.RegisterInterface((*exported.Auction)(nil), nil)
+	cdc.RegisterConcrete(&SurplusAuction{}, "auction/SurplusAuction", nil)
+	cdc.RegisterConcrete(&DebtAuction{}, "auction/DebtAuction", nil)
+	cdc.RegisterConcrete(&CollateralAuction{}, "auction/CollateralAuction", nil)
 }
 
-// RegisterCodec registers concrete types on the codec.
-func RegisterCodec(cdc *codec.Codec) {
-	cdc.RegisterConcrete(MsgPlaceBid{}, "auction/MsgPlaceBid", nil)
+func RegisterInterfaces(registry types.InterfaceRegistry) {
+	registry.RegisterImplementations((*sdk.Msg)(nil),
+		&MsgPlaceBid{},
+	)
 
-	cdc.RegisterInterface((*GenesisAuction)(nil), nil)
-	cdc.RegisterInterface((*Auction)(nil), nil)
-	cdc.RegisterConcrete(SurplusAuction{}, "auction/SurplusAuction", nil)
-	cdc.RegisterConcrete(DebtAuction{}, "auction/DebtAuction", nil)
-	cdc.RegisterConcrete(CollateralAuction{}, "auction/CollateralAuction", nil)
+	msgservice.RegisterMsgServiceDesc(registry, &_Msg_serviceDesc)
+}
+
+var (
+	amino = codec.NewLegacyAmino()
+
+	// ModuleCdc references the global x/gov module codec. Note, the codec should
+	// ONLY be used in certain instances of tests and for JSON encoding as Amino is
+	// still used for that purpose.
+	//
+	// The actual codec used for serialization should be provided to x/gov and
+	// defined at the application level.
+	ModuleCdc = codec.NewAminoCodec(amino)
+)
+
+func init() {
+	RegisterLegacyAminoCodec(amino)
+	cryptocodec.RegisterCrypto(amino)
 }
