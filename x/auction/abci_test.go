dcdb73e6b81cf028d90ce8009636a19cbb65a603
denalimarsh <denalimarsh@gmail.com>
2021-11-10 11:12:45 -0800

    comment out test for compile

--- x/auction/abci_test.go
+++ x/auction/abci_test.go
@@ -1,69 +1,75 @@
 package auction_test
 
-// import (
-// 	"testing"
+import (
+	"testing"
 
-// 	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/require"
 
-// 	sdk "github.com/cosmos/cosmos-sdk/types"
-// 	"github.com/cosmos/cosmos-sdk/x/auth"
-// 	authexported "github.com/cosmos/cosmos-sdk/x/auth/exported"
-// 	"github.com/cosmos/cosmos-sdk/x/supply"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
 
-// 	abci "github.com/tendermint/tendermint/abci/types"
+	"github.com/kava-labs/kava/app"
+	"github.com/kava-labs/kava/x/auction"
+	types "github.com/kava-labs/kava/x/auction/types"
+)
 
-// 	"github.com/kava-labs/kava/app"
-// 	"github.com/kava-labs/kava/x/auction"
-// 	"github.com/kava-labs/kava/x/cdp"
-// )
+func TestKeeper_BeginBlocker(t *testing.T) {
+	// Setup
+	_, addrs := app.GeneratePrivKeyAddressPairs(2)
+	buyer := addrs[0]
+	returnAddrs := addrs[1:]
+	var returnAddrStrs []string
+	for _, addr := range returnAddrs {
+		returnAddrStrs = append(returnAddrStrs, addr.String())
+	}
 
-// func TestKeeper_BeginBlocker(t *testing.T) {
-// 	// Setup
-// 	_, addrs := app.GeneratePrivKeyAddressPairs(2)
-// 	buyer := addrs[0]
-// 	returnAddrs := addrs[1:]
-// 	returnWeights := []sdk.Int{sdk.NewInt(1)}
-// 	sellerModName := cdp.LiquidatorMacc
+	returnWeights := []sdk.Int{sdk.NewInt(1)}
 
-// 	tApp := app.NewTestApp()
-// 	sellerAcc := supply.NewEmptyModuleAccount(sellerModName)
-// 	require.NoError(t, sellerAcc.SetCoins(cs(c("token1", 100), c("token2", 100), c("debt", 100))))
-// 	tApp.InitializeFromGenesisStates(
-// 		NewAuthGenStateFromAccs(authexported.GenesisAccounts{
-// 			auth.NewBaseAccount(buyer, cs(c("token1", 100), c("token2", 100)), nil, 0, 0),
-// 			sellerAcc,
-// 		}),
-// 	)
+	// TODO: update to cdp.LiqMacc
+	sellerModName := "liquidator"
 
-// 	ctx := tApp.NewContext(true, abci.Header{})
-// 	keeper := tApp.GetAuctionKeeper()
+	tApp := app.NewTestApp()
+	ctx := tApp.NewContext(true, tmproto.Header{Height: 1})
 
-// 	// Start an auction and place a bid
-// 	auctionID, err := keeper.StartCollateralAuction(ctx, sellerModName, c("token1", 20), c("token2", 50), returnAddrs, returnWeights, c("debt", 40))
-// 	require.NoError(t, err)
-// 	require.NoError(t, keeper.PlaceBid(ctx, auctionID, buyer, c("token2", 30)))
+	sellerAcc := authtypes.NewEmptyModuleAccount(sellerModName)
+	err := tApp.FundModuleAccount(ctx, sellerAcc.Name, cs(c("token1", 100), c("token2", 100), c("debt", 100)))
+	require.NoError(t, err)
+	tApp.InitializeFromGenesisStates(
+		NewAuthGenStateFromAccs(authtypes.GenesisAccounts{
+			authtypes.NewBaseAccount(buyer, nil, 0, 0),
+			sellerAcc,
+		}),
+	)
 
-// 	// Run the beginblocker, simulating a block time 1ns before auction expiry
-// 	preExpiryTime := ctx.BlockTime().Add(auction.DefaultBidDuration - 1)
-// 	auction.BeginBlocker(ctx.WithBlockTime(preExpiryTime), keeper)
+	keeper := tApp.GetAuctionKeeper()
 
-// 	// Check auction has not been closed yet
-// 	_, found := keeper.GetAuction(ctx, auctionID)
-// 	require.True(t, found)
+	// Start an auction and place a bid
+	auctionID, err := keeper.StartCollateralAuction(ctx, sellerModName, c("token1", 20), c("token2", 50), returnAddrStrs, returnWeights, c("debt", 40))
+	require.NoError(t, err)
+	require.NoError(t, keeper.PlaceBid(ctx, auctionID, buyer, c("token2", 30)))
 
-// 	// Run the endblocker, simulating a block time equal to auction expiry
-// 	expiryTime := ctx.BlockTime().Add(auction.DefaultBidDuration)
-// 	auction.BeginBlocker(ctx.WithBlockTime(expiryTime), keeper)
+	// Run the beginblocker, simulating a block time 1ns before auction expiry
+	preExpiryTime := ctx.BlockTime().Add(types.DefaultBidDuration - 1)
+	auction.BeginBlocker(ctx.WithBlockTime(preExpiryTime), keeper)
 
-// 	// Check auction has been closed
-// 	_, found = keeper.GetAuction(ctx, auctionID)
-// 	require.False(t, found)
-// }
+	// Check auction has not been closed yet
+	_, found := keeper.GetAuction(ctx, auctionID)
+	require.True(t, found)
 
-// func c(denom string, amount int64) sdk.Coin { return sdk.NewInt64Coin(denom, amount) }
-// func cs(coins ...sdk.Coin) sdk.Coins        { return sdk.NewCoins(coins...) }
+	// Run the endblocker, simulating a block time equal to auction expiry
+	expiryTime := ctx.BlockTime().Add(types.DefaultBidDuration)
+	auction.BeginBlocker(ctx.WithBlockTime(expiryTime), keeper)
 
-// func NewAuthGenStateFromAccs(accounts authexported.GenesisAccounts) app.GenesisState {
-// 	authGenesis := auth.NewGenesisState(auth.DefaultParams(), accounts)
-// 	return app.GenesisState{auth.ModuleName: auth.ModuleCdc.MustMarshalJSON(authGenesis)}
-// }
+	// Check auction has been closed
+	_, found = keeper.GetAuction(ctx, auctionID)
+	require.False(t, found)
+}
+
+func c(denom string, amount int64) sdk.Coin { return sdk.NewInt64Coin(denom, amount) }
+func cs(coins ...sdk.Coin) sdk.Coins        { return sdk.NewCoins(coins...) }
+
+func NewAuthGenStateFromAccs(accounts authtypes.GenesisAccounts) app.GenesisState {
+	authGenesis := authtypes.NewGenesisState(authtypes.DefaultParams(), accounts)
+	return app.GenesisState{authtypes.ModuleName: authtypes.ModuleCdc.MustMarshalJSON(authGenesis)}
+}
